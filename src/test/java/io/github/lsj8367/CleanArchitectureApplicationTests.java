b6c29cd492f68e1ffddf124c229d5250971dcf37
SeungJae Lee <lsj8367@naver.com>
2021-12-01 09:56:42 +0900

    Initial commit

---
+++ src/test/java/io/github/lsj8367/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lsj8367;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
